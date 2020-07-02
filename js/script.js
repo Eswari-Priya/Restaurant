f700ed9210905f2c0542a7fd6b4a80e30d8c9c2f
Eswari-Priya <repakaeswaripriya@gmail.com>
2020-07-02 18:19:31 +0530

    npm scripts

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+    $("#reservebtn").click(function () {
+        $("#reserve-table").modal("show");
+    });
+    $("#Loginbtn").click(function(event) {
+        $("#loginModal").modal("show");
+    });
+});
